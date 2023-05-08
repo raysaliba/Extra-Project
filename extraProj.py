4c6434a1a3bd1b03bbb918e02088e6beab6f0a3a
Raymond Saliba <raymondsaliba@Raymonds-MacBook-Pro.local>
2023-05-08 22:01:12 +0300

    made the script select fall 2023 and submit it

--- extraProj.py
+++ extraProj.py
@@ -1,6 +1,7 @@
 import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
 
 options = webdriver.ChromeOptions()
 options.add_argument("--headless")
@@ -22,3 +23,9 @@ courses_btn.click()
 course_offerings_btn = driver.find_element(By.XPATH, '//[@id="cbqwpctl00_m_g_df4e4cc9_291f_48af_90eb_524811091537"]/div/div[2]/a[4]')
 course_offerings_btn.click()
 driver.get("https://banweb.lau.edu.lb/prod/bwckschd.p_disp_dyn_sched")
+
+select_term_box = driver.find_element(By.XPATH, '//[@id="term_input_id"]')
+select_term = Select(select_term_box)
+select_term.select_by_value("202410")
+submit_btn = driver.find_element(By.XPATH, "/html/body/div[3]/form/input[2]")
+submit_btn.click()
